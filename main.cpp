c6b5e2b74597186d8f4b576071801705426811d3
Duducamposs <camposluizeduardo03@gmail.com>
2026-03-27 20:26:06 -0300

    teste 3

--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
 #include<iostream>
 
 int main(){
+
+    int num1 = 8;
+    int num2 = 2;
  
     std::cout<<8+2<<std::endl;
     std::cout<<10-4<<std::endl;
     std::cout<<4*3<<std::endl;
     std::cout<<8/2<<std::endl;  
 
-    
+
     return 0;
 
 }
