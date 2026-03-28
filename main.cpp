973af30ba704649d570d410c402ee9a3e3b1f758
Duducamposs <camposluizeduardo03@gmail.com>
2026-03-27 21:08:55 -0300

    teste 4

--- main.cpp
+++ main.cpp
@@ -1,20 +1,14 @@
 #include<iostream>
 
-int main(){
+int main(){         
 
-    int num1 = 8;
-    int num2 = 2;
-    int soma, subt, mult, div;
+    int idade = 23;
+    float altura= 1.67;
+    std::string nome = "eduardo";
 
-    soma = num1 + num2;
-    subt = num1 - num2;
-    mult = num1 * num2;
-    div = num1 / num2;
-
-    std::cout<<soma<<std::endl;
-    std::cout<<subt<<std::endl;
-    std::cout<<mult<<std::endl;
-    std::cout<<div<<std::endl;
+    std::cout<<"meu nome é"<<nome<<"."<<std::endl;
+    std::cout<<"minha altura é"<<altura<<"m"<<std::endl;
+    std::cout<<"minha idade é"<<idade<<"anos"<<std::endl;
 
 
     return 0;
