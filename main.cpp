f5fb2f84c9636243e16662b673279a271c51f1ab
Duducamposs <camposluizeduardo03@gmail.com>
2026-03-27 21:59:41 -0300

    teste 5

--- main.cpp
+++ main.cpp
@@ -4,10 +4,24 @@
 int main(){         
 
     std::string nome;
+    int idade;
+    float altura;
 
-    std::cout<<"Qual seu nome: "<<std::endl;
+
+    std::cout<<"qual seu nome?"<<std::endl;
     std::cin>>nome;
-    std::cout<<"Bem-vindo(a) "<<nome<<"!!"<<std::endl; 
+
+    std::cout<<"qual sua idade?"<<std::endl;
+    std::cin>>idade;
+
+    std::cout<<"qual sua altura"<<std::endl;
+    std::cin>>altura; 
+
+
+
+    std::cout<<"Seu nome é "<<" nome"<<"!!"<<std::endl;
+    std::cout<<"Sua idade é "<<" idade"<<"!!"<<std::endl;
+    std::cout<<"Sua altura é "<<" altura"<<"!!"<<std::endl;
 
 
 
