fabe4ac77408bc4e1b6f1ff1b3d13d73d97b0224
Duducamposs <camposluizeduardo03@gmail.com>
2026-04-15 19:58:41 -0300

    A bomba vai explodir em... só que agora com os segundos

--- main.cpp
+++ main.cpp
@@ -1,19 +1,19 @@
 #include<iostream>
 #include<windows.h>
 #include<string>
-#include<thread> //para lidar com o tempo de execução 
-#include<chrono> //para definir unidades de tempo(ms, ssegundos)
+
 int main(){
 
     SetConsoleOutputCP(CP_UTF8);//
     SetConsoleCP(CP_UTF8);
 
-    for(int i = 10; i >= 0; i--){
-            std::cout<<"A bomba vai explodir em.."<<i<<std::endl;
+    int tabuada = 7;
 
-            std::this_thread::sleep_for(std::chrono::milliseconds(5000));
-            //secunds -> millisecunds
-    } 
+    for(int i = 0; i <= 10; i++){
+        std::cout<<"O resultado de "<<tabuada<<" x "<<i<<" = "<<tabuada * i<<std::endl;
+            /* code */
+    }
+    
 
    
     return 0;
