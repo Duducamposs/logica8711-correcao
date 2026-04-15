488d6e361b0f31d151db0082151fe9f3a349fba8
Duducamposs <camposluizeduardo03@gmail.com>
2026-04-15 20:04:51 -0300

    tabuada

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ int main(){
     int tabuada = 7;
 
     for(int i = 0; i <= 10; i++){
+        
         std::cout<<"O resultado de "<<tabuada<<" x "<<i<<" = "<<tabuada * i<<std::endl;
             /* code */
     }
