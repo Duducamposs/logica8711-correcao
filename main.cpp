e254ad7717d499b3deac0cd13a81b5006f73c9a7
Duducamposs <camposluizeduardo03@gmail.com>
2026-04-30 20:58:03 -0300

    par ou impar

--- main.cpp
+++ main.cpp
@@ -4,26 +4,23 @@
 
 
    int main(){
-    //brAmount: valor em reais / exchangesRate: taxa de Cãmbio / usdAmount: Valor em dólar
-    double brAmount, exchangeRate, usdAmount;
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
 
-    std::cout<<"----- Currency Convertee: BRL to USD -----"<<std::endl;
+    int num;
 
-    //entrada da taxa de cãmbio (quanto vale 1 dolar hoje)
-    std::cin>>exchangeRate;
 
-    //Entrada da quantidade em Reais que a pessoa deseja converter
-    std::cout<<"Enter the amount in BRL(R$): "<<std::endl;
-    std::cin>>brAmount;
+    std::cout<<"Digite um Número "<<std::endl;
+    std::cin>>num;
 
-    //verificação lógica; garante que o valor informado seja positivo
-    if(brAmount > 0){
-        //cãlculo; dividi o montante em reais pela taxa de cãmbio
-        usdAmount =brAmount / exchangeRate;
-        std::cout<<"you will have $"<<usdAmount<<"US dollars. "<<std::endl;
+    std::cout<<num%2<<std::endl;
+
+    if(num % 2 == 0){
+        std::cout<<num<<" é par!"<<std::endl;
     }else{
-        //Mensagem de erro caso o valor sja zero ou negativo
-        std::cout<<"Error: Please enter a valid amount greater than zero. "<<std::endl;
+        std::cout<<num<<" é impar!!"<<std::endl;
     }
+   
+   
     return 0;
     }
